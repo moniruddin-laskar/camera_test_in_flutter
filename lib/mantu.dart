ee06f0a06554e1a4ffbd01aa72cdd07076e4ea35
SanchitaM2 <sanchita.dhara3@gmail.com>
2023-03-01 10:36:16 +0530

    add a file named mantu.dart

---
+++ lib/mantu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MantuData extends StatefulWidget {
+  const MantuData({super.key});
+
+  @override
+  State<MantuData> createState() => _MantuDataState();
+}
+
+class _MantuDataState extends State<MantuData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
